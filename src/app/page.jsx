54ba34a4faeb1c95b9597df08f47ceef94a6d913
kreifeur <kreifeur669@gmail.com>
2024-10-18 10:23:51 +0200

    in

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,15 +3,19 @@ import image from "../../public/remorquage.jpg";
 import image1 from "../../public/remorquage.jpg";
 import image2 from "../../public/logo-republique-francaise.png";
 import image3 from "../../public/remorquage.jpg";
-import image5 from "../../public/logo-republique-francaise.png";
+import image5 from "../../public/iledefrance.gif";
 import { FaArrowRightLong } from "react-icons/fa6";
 import image4 from "../../public/eco.png";
+import { RiStarSFill } from "react-icons/ri";
 
 import { FaPhone } from "react-icons/fa";
+import { GiTowTruck } from "react-icons/gi";
+import { FaBuilding } from "react-icons/fa";
+import { HiMiniUserGroup } from "react-icons/hi2";
 
 export default function Home() {
   return (
-    <div>
+    <div className="">
       <div
         className="flex h-[79vh] bg-cover bg-center relative flex items-center p-4 gap-[8vh]"
         style={{ backgroundImage: `url(${image3.src})` }}
@@ -50,111 +54,171 @@ export default function Home() {
           </div>
         </div>
       </div>
-      <div className="">
-        {/* <Image
-          src={image1.src}
-          width={1000}
-          height={1000}
-          style={{ width: "100%", height: "auto" }}
-        /> */}
 
-        {/* section 1 */}
-        <div className="flex p-8 sm:h-[90vh] flex-col sm:flex-row gap-6">
-          <div className="sm:w-[50%]  flex flex-col gap-4 items-center justify-center">
-            <div className="sm:w-[70%] flex flex-col gap-4">
-              <div className="font-bold text-2xl uppercase">
-                Qui sommes-nous ?
-              </div>
-              <div className="text-md">
-                ALLO RECYCLAGE VHU agrée est une équipe de professionnels
-                confirmés, spécialisée dans l’enlèvement d’épave et le recyclage
-                de tous types de véhicules à Paris et sa banlieue depuis plus de
-                15 ans. <br /> Nous disposons d’une expérience significative
-                dans le recyclage des métaux, ainsi que le dépannage et
-                l’assistance de voitures. <br />
-                Les maîtres-mots de la satisfaction des clients.
-              </div>
-              <div className="font-bold flex gap-2 items-center">
-                FIND OUT MORE{" "}
-                <FaArrowRightLong className="text-[#03a776] text-md" />
-              </div>
-            </div>
+      {/* statistique
+       */}
+      <div className="w-full min-h-[200px] flex gap-[7vh] justify-center sm:justify-around items-center flex-col sm:flex-row  p-8">
+        <div className="flex flex-col gap-2 items-center">
+          <div>
+            <RiStarSFill className="text-4xl text-[#03a776]" />
           </div>
-          <div className="sm:w-[50%] h-full flex items-center sm:justify-center">
-            <Image
-              width={1000}
-              height={1000}
-              style={{ width: "100%", height: "auto" }}
-              src={image1.src}
-              alt=""
-              srcset=""
-            />
-          </div>
+          <div className="text-4xl font-bold ">10</div>
+          <div>Ans dexpériense</div>
         </div>
 
-        {/* band image */}
-        {/* <img className="w-full h-[50vh]" src={image3.src} alt="" srcset="" /> */}
-        <div
-          className="w-full min-h-[500px] bg-cover bg-center flex p-8 flex items-center"
-          style={{ backgroundImage: `url(${image3.src})` }}
-        >
-          <div className="min-h-full sm:w-[50%] w-full bg-white sm:px-[10vh] p-8 flex flex-col justify-center gap-8 border-t border-t-[15px] border-t-[#03a776] ">
-            <div className="font-bold text-3xl uppercase ">
-              L’écologie du recyclage :
+        <div className="flex flex-col gap-2 items-center">
+          <div>
+            <FaBuilding className="text-4xl text-[#03a776]" />
+          </div>
+          <div className="text-4xl font-bold ">10</div>
+          <div>Ans dexpériense</div>
+        </div>
+
+        <div className="flex flex-col gap-2 items-center">
+          <div>
+            <GiTowTruck className="text-4xl text-[#03a776]" />
+          </div>
+          <div className="text-4xl font-bold ">10</div>
+          <div>Ans dexpériense</div>
+        </div>
+
+        <div className="flex flex-col gap-2 items-center">
+          <div>
+            <HiMiniUserGroup className="text-4xl text-[#03a776]" />
+          </div>
+          <div className="text-4xl font-bold ">10</div>
+          <div>Ans dexpériense</div>
+        </div>
+      </div>
+
+      {/* section 1 */}
+      <div className="flex p-8 sm:h-[90vh] flex-col sm:flex-row gap-6">
+        <div className="sm:w-[50%]  flex flex-col gap-4 items-center justify-center">
+          <div className="sm:w-[70%] flex flex-col gap-4">
+            <div className="font-bold text-2xl uppercase">
+              Qui sommes-nous ?
             </div>
             <div className="text-md">
-              Enjeu crucial du moment, la protection de l’environnement est
-              désormais une priorité collective et sociale face à l’urgence
-              climatique. Le recyclage des métaux réduit considérablement les
-              émissions de gaz à effet de serre en diminuant les nouvelles
-              demandes de matières premières et prévient la pollution associée
-              aux activités minières intensives. Il permet également la
-              protection des sols et des nappes phréatiques des substances
-              nocives contenues dans les métaux usagers.
+              ALLO RECYCLAGE VHU agrée est une équipe de professionnels
+              confirmés, spécialisée dans l’enlèvement d’épave et le recyclage
+              de tous types de véhicules à Paris et sa banlieue depuis plus de
+              15 ans. <br /> Nous disposons d’une expérience significative dans
+              le recyclage des métaux, ainsi que le dépannage et l’assistance de
+              voitures. <br />
+              Les maîtres-mots de la satisfaction des clients.
+            </div>
+            <div className="font-bold flex gap-2 items-center">
+              FIND OUT MORE{" "}
+              <FaArrowRightLong className="text-[#03a776] text-md" />
+            </div>
+          </div>
+        </div>
+        <div className="sm:w-[50%] h-full flex items-center sm:justify-center">
+          <Image
+            width={1000}
+            height={1000}
+            style={{ width: "100%", height: "auto" }}
+            src={image1.src}
+            alt=""
+            srcset=""
+          />
+        </div>
+      </div>
+
+      {/* band image */}
+      {/* <img className="w-full h-[50vh]" src={image3.src} alt="" srcset="" /> */}
+      <div
+        className="w-full min-h-[500px] bg-cover bg-center flex p-8 flex items-center"
+        style={{ backgroundImage: `url(${image3.src})` }}
+      >
+        <div className="min-h-full sm:w-[50%] w-full bg-white sm:px-[10vh] p-8 flex flex-col justify-center gap-8 border-t border-t-[15px] border-t-[#03a776] ">
+          <div className="font-bold text-3xl uppercase ">
+            L’écologie du recyclage :
+          </div>
+          <div className="text-md">
+            Enjeu crucial du moment, la protection de l’environnement est
+            désormais une priorité collective et sociale face à l’urgence
+            climatique. Le recyclage des métaux réduit considérablement les
+            émissions de gaz à effet de serre en diminuant les nouvelles
+            demandes de matières premières et prévient la pollution associée aux
+            activités minières intensives. Il permet également la protection des
+            sols et des nappes phréatiques des substances nocives contenues dans
+            les métaux usagers.
+          </div>
+        </div>
+      </div>
+
+      {/* section 2 */}
+      <div className="flex sm:flex-row-reverse p-8 sm:h-[90vh] flex-col sm:flex-row gap-6">
+        <div className="sm:w-[50%]  flex flex-col gap-4 items-center justify-center">
+          <div className="sm:w-[70%] flex flex-col gap-4">
+            <div className="font-bold text-2xl">
+              WHY <span className="text-orange-500">ALLO DEPANNAGE</span> !
+            </div>
+            <div className="text-xl">
+              Epaviste Gratuit IDF est là pour s’occuper de vos besoins
+              d’enlèvement d’épave gratuit à Paris à tout moment, n’importe quel
+              jour. Nous prenons en charge toutes les tâches liées à ce
+              processus difficile.
+            </div>
+            <div className="font-bold flex gap-2 items-center">
+              FIND OUT MORE{" "}
+              <FaArrowRightLong className="text-[#03a776] text-md" />
+            </div>
+          </div>
+        </div>
+        <div className="sm:w-[50%] h-full flex items-center sm:justify-center">
+          <Image
+            width={1000}
+            height={1000}
+            style={{ width: "100%", height: "auto" }}
+            src={image.src}
+            alt=""
+            srcset=""
+          />
+        </div>
+      </div>
+
+      {/* region d'intervention 1 */}
+      <div className="flex p-8  flex-col sm:flex-row gap-6">
+        <div className="sm:w-[50%]  flex flex-col gap-4 items-center justify-center bg-[#03a776] text-white p-8">
+          <div className=" flex flex-col  items-center">
+            <div className="font-bold text-2xl uppercase mb-1">
+              Ile de France
+            </div>
+            <div className="mb-5 font-bold">BESOIN D’AIDE EN URGENCE ?</div>
+            <div className="text-md">
+              Recycle-Epave.fr vous proposent leurs services d’elevement d’épave
+              à Paris et dans toute la région Parisienne (75, 91, 92, 932, 94,
+              95, 77, 78). Nous sommes présents dans toute la région parisienne
+              ainsi que dans les départements limitrophes. Si vous envisagez de
+              récupérer votre moto ou votre véhicule, n’hésitez pas à faire
+              appel à nos services (60, 27, 28, 45, 89, 02…).
             </div>
           </div>
         </div>
 
-        {/* section 2 */}
-        <div className="flex sm:flex-row-reverse p-8 sm:h-[90vh] flex-col sm:flex-row gap-6">
-          <div className="sm:w-[50%]  flex flex-col gap-4 items-center justify-center">
-            <div className="sm:w-[70%] flex flex-col gap-4">
-              <div className="font-bold text-2xl">
-                WHY <span className="text-orange-500">ALLO DEPANNAGE</span> !
-              </div>
-              <div className="text-xl">
-                Epaviste Gratuit IDF est là pour s’occuper de vos besoins
-                d’enlèvement d’épave gratuit à Paris à tout moment, n’importe
-                quel jour. Nous prenons en charge toutes les tâches liées à ce
-                processus difficile.
-              </div>
-              <div className="font-bold flex gap-2 items-center">
-                FIND OUT MORE{" "}
-                <FaArrowRightLong className="text-[#03a776] text-md" />
-              </div>
-            </div>
-          </div>
-          <div className="sm:w-[50%] h-full flex items-center sm:justify-center">
-            <Image
-              width={1000}
-              height={1000}
-              style={{ width: "100%", height: "auto" }}
-              src={image.src}
-              alt=""
-              srcset=""
-            />
-          </div>
+        <div className="sm:w-[50%] h-full flex items-center justify-center p-4">
+          <Image
+            width={200}
+            height={200}
+            className="sm:w-[60%] h-auto w-full"
+            src={image5.src}
+            alt=""
+            srcset=""
+          />
         </div>
-        {/* band */}
-        <div
-          className="w-full h-[200px] bg-cover bg-center flex p-8 flex items-center relative z-[1]"
-          style={{ backgroundImage: `url(${image.src})` }}
-        >
-          <div className="absolute inset-0 w-full h-full bg-[rgba(0,0,0,0.7)] text-white flex flex-col items-center justify-center gap-4 z-[1]">
-            <div className="font-bold text-3xl">CONTACT US NOW</div>
-            <div className="px-6 py-3 bg-[#03a776] font-bold flex items-center gap-2">
-              <FaPhone className="text-xl" /> Call now{" "}
-            </div>
+      </div>
+
+      {/* band */}
+      <div
+        className="w-full h-[200px] bg-cover bg-center flex p-8 flex items-center relative z-[1]"
+        style={{ backgroundImage: `url(${image.src})` }}
+      >
+        <div className="absolute inset-0 w-full h-full bg-[rgba(0,0,0,0.7)] text-white flex flex-col items-center justify-center gap-4 z-[1]">
+          <div className="font-bold text-3xl">CONTACT US NOW</div>
+          <div className="px-6 py-3 bg-[#03a776] font-bold flex items-center gap-2">
+            <FaPhone className="text-xl" /> Call now{" "}
           </div>
         </div>
       </div>
